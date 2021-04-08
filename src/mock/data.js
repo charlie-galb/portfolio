094ca7a01f650b5fd5595a9239ccf953b71bd02d
charlie-galb <galbraith.charlie@gmail.com>
2021-04-08 09:55:14 +0100

    Add custom subtitle

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,16 +11,16 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Charlie',
-  subtitle: '',
+  subtitle: 'Charlie who?',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'Profile_pic.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne: 'I\'m a full-stack developer and Makers graduate with a background in publishing and English-language teaching.',
+  paragraphTwo: 'I was drawn to programming by curiosity. I was tired of staring at screens and wondering about the magic that made the pictures move around, so I decided to find out. As soon as I started, I was hooked. The cycle of frustration and triumph in programming is more addictive than any puzzle game, and the sense of satisfaction that comes from carrying through a project from conception to deployment is unparalleled.',
+  paragraphThree: 'Have a look at my projects and get in touch if you\'re interested in collaborating, or if you just want to hurl some abuse my way (I\'ve been online before, you know).',
   resume: 'https://github.com/charlie-galb/CV/blob/master/README.md', // if no resume, the button will not show up
 };
 
@@ -29,28 +29,28 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'Tape-It-UI.png',
-    title: '',
-    info: '',
+    title: 'Taped It',
+    info: 'A voice-notes plug-in for slack.',
     info2: '',
-    url: '',
+    url: 'https://tapedit.netlify.app/',
     repo: 'https://github.com/voice-notes', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'le-chicken-crop.png',
-    title: '',
-    info: '',
-    info2: '',
+    title: 'Le Chicken: The Poulet',
+    info: 'A language-learning RPG built in React Native.',
+    info2: 'This was a group project completed over the course of two weeks while at Makers Academy.',
     url: '',
     repo: 'https://github.com/emilyalice2708/le-chicken', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'chitter-homescreen.png',
-    title: '',
-    info: '',
+    title: 'Chitter',
+    info: 'A social media platform.',
     info2: '',
-    url: '',
+    url: 'https://chitter-deployed.netlify.app/',
     repo: 'https://github.com/charlie-galb/chitter-frontend-react', // if no repo, the button will not show up
   },
 ];
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'galbraith.charlie@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +67,18 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/charliegalbraith/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/charlie-galb',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
